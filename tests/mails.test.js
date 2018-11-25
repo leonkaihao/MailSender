a90d45d5589765e842cdcbb835a20b009991e5d5
Hao Kai <leonkaihao@outlook.com>
2018-11-26 10:54:45 +1100

    Add test cases

---
+++ tests/mails.test.js
@@ -0,0 +1,31 @@
+let expect = require("chai").expect;
+let mails = require("../services/mails");
+let mailsCtrl = require("../controllers/mails");
+let sessions = require("../services/sessions");
+
+describe("test providers", function() {    
+    it("not null provider", function() {
+        mails.getProviders(function(err, data) {
+            expect(err).to.be.null;
+            expect(data.length).to.be.equal(2);
+        });
+    });
+});
+
+describe("test add sender", function() {    
+    it("normal", function() {
+        let token = sessions.create();
+        mailsCtrl.updateSenders(token, [
+            {
+                name: "SendGrid", 
+                "apiKey": "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",
+                "domain": null
+            }
+        ], function(code, result) {
+            expect(code).equal(200);
+            let content = sessions.getContent(token);
+            expect(content.senders[0].name).to.be.equal("SendGrid");
+        });
+
+    });
+});
